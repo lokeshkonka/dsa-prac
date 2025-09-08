72e7e1dc11859a06638534cebb382276431ec036
lokeshkonka <konkalokesh372@gmail.com>
2025-09-08 21:32:06 +0530

    basic

---
+++ basic.cpp
@@ -0,0 +1,49 @@
+// #include<iostream>
+// #include <bits/stdc++.h>
+// int main(){
+//     std::cout << "HEllO WORLD" << "\n";
+//     std::cout << "HEllO WORLD";
+
+//     return 0;
+// }
+// cin to take input from the terminal
+
+// #include<bits/stdc++.h>
+// using namespace std;
+
+
+// int main(){
+//     cout << "Using namespace";
+    
+//     //integer
+//     int x =0;
+//     //long
+//     long y ;
+    
+//     cin>>y;
+//     cout<<"The value of y is :"<< y;
+    
+//     //very long integer
+//     long long y;
+    
+// }
+
+//float for float
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    //string
+    // string s1;
+    // string s2;
+    // //input hey loki
+    // cin >> s1 >> s2;
+    // cout<< s1 << " " << s2;
+    /// Totake Entire Line
+    string str;
+    getline(cin,str);
+    cout<< "  "<< str; 
+    return 0;
+    
+}
