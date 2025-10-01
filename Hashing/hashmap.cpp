e167258321845982394b250dd910d995335c983c
Lokesh Konka <konkalokesh372@gmail.com>
2025-10-01 20:08:48 +0600

    Collisions and hashmaps

--- Hashing/hashmap.cpp
+++ Hashing/hashmap.cpp
@@ -11,6 +11,8 @@ int main(){
 //..precompute 
 //all are stored in sorted in map
 map<int , int>mpp;
+//unordered_map<int , int>mpp;
+/// for unordered the avg and best case is O(1) ,Worst Case is O(N) for no of elements ,bcuz of internal collisions
 for(int i=0; i<n; i++){
     mpp[arr[i]]++;
 }
@@ -42,3 +44,26 @@ cout << mpp[key] << endl;
     return 0;
 
 }
+/*
+Hasing -> Division Method, Folding Method, Mid square Method
+-Divison Method -> key%table_size
+-Collision -> when two keys are mapped to the same index
+-Open Addressing -> Linear Probing, Quadratic Probing, Double Hashing
+-Chaining -> Linked List, BST, AVL Tree
+-Load Factor -> number of elements / table size
+
+[2,5,28,38]
+ 2%10 = 2
+ 5%10 = 5
+ 28%10 = 8
+
+0
+1
+2->2
+5 ->5
+8->28,38
+
+.
+.
+
+*/
